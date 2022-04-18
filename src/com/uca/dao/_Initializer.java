f150f3e1dd71399d655f2591d641299ed788feda
Reynalde SEGERIE <Reynalde Segerie>
2022-04-18 13:50:41 +0200

    Add Insert Into (_Initializer)

--- src/com/uca/dao/_Initializer.java
+++ src/com/uca/dao/_Initializer.java
@@ -11,6 +11,17 @@ public class _Initializer {
             PreparedStatement statement;
 
             //Init articles table
+            /** Drop table, uncomment to use / comment to desactivate **/
+            /*
+            statement = connection.prepareStatement("DROP TABLE studentsStickers");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("DROP TABLE teachers");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("DROP TABLE students");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("DROP TABLE stickers");
+            statement.executeUpdate();*/
+            /**                                                     **/
 
             // Students table //
             statement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS students (id_student int primary key auto_increment, firstname varchar(100), lastname varchar(100), student_class varchar(50)); ");
@@ -31,6 +42,73 @@ public class _Initializer {
             statement.executeUpdate();
 
 
+            /** Insert into, uncomment to use / comment to desactivate **/
+
+            /** Students **/
+            /*
+            statement = connection.prepareStatement("INSERT INTO students(firstname, lastname, student_class) VALUES(?, ?, ?);");
+            statement.setString(1, "Theodore");
+            statement.setString(2, "Muillerez");
+            statement.setString(3, "C1");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO students(firstname, lastname, student_class) VALUES(?, ?, ?);");
+            statement.setString(1, "Mathilde");
+            statement.setString(2, "Richard");
+            statement.setString(3, "C1");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO students(firstname, lastname, student_class) VALUES(?, ?, ?);");
+            statement.setString(1, "Louis");
+            statement.setString(2, "Raymond");
+            statement.setString(3, "C4");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO students(firstname, lastname, student_class) VALUES(?, ?, ?);");
+            statement.setString(1, "Mathias");
+            statement.setString(2, "Bernard");
+            statement.setString(3, "C2");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO students(firstname, lastname, student_class) VALUES(?, ?, ?);");
+            statement.setString(1, "Julie");
+            statement.setString(2, "Roberts");
+            statement.setString(3, "C2");
+            statement.executeUpdate();*/
+
+            /** Stickers **/
+            /*
+            statement = connection.prepareStatement("INSERT INTO stickers(description, color) VALUES(?, ?);");
+            statement.setString(1, "Mauvais comportement");
+            statement.setString(2, "ROUGE");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO stickers(description, color) VALUES(?, ?);");
+            statement.setString(1, "Bon travail");
+            statement.setString(2, "VERT");
+            statement.executeUpdate();
+            statement = connection.prepareStatement("INSERT INTO stickers(description, color) VALUES(?, ?);");
+            statement.setString(1, "Bonne participation");
+            statement.setString(2, "BLEU");
+            statement.executeUpdate();*/
+            /** Teachers **/
+            /*
+            statement = connection.prepareStatement("INSERT INTO teachers(firstname, lastname, username, password) VALUES(?, ?, ?, ?);");
+            statement.setString(2, "Myrtille");
+            statement.setString(1, "Mireille");
+            statement.setString(3, "myrmirei");
+            statement.setString(4, "myr15");
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("INSERT INTO teachers(firstname, lastname, username, password) VALUES(?, ?, ?, ?);");
+            statement.setString(2, "Framboise");
+            statement.setString(1, "Julien");
+            statement.setString(3, "julframb");
+            statement.setString(4, "jufram15");
+            statement.executeUpdate();
+
+            statement = connection.prepareStatement("INSERT INTO teachers(firstname, lastname, username, password) VALUES(?, ?, ?, ?);");
+            statement.setString(2, "Jouveneau");
+            statement.setString(1, "Bruno");
+            statement.setString(3, "brunojouv");
+            statement.setString(4, "bbj15");
+            statement.executeUpdate();*/
+            /**                                     **/
 
 
         } catch (Exception e){
